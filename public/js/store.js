48a0d44693395d72dbaf70495cc7d7a3bb323179
Sam Gould <sjg326@gmail.com>
2018-01-03 09:49:47 -0800

    updated store.js file to limit potential functionality

--- public/js/store.js
+++ public/js/store.js
@@ -8,25 +8,25 @@ class Store {
     this.query = null;
   }
 
-  findById(id) {
-    return this.data.find(item => item.id === Number(id));
-  }
+  // findById(id) {
+  //   return this.data.find(item => item.id === Number(id));
+  // }
 
-  findByIdAndRemove(id) {
-    const index = this.data.findIndex(item => item.id === Number(id));
-    if (index !== -1) {
-      return this.data.splice(index, 1).length;
-    } else {
-      return 0;
-    }
-  }
+  // findByIdAndRemove(id) {
+  //   const index = this.data.findIndex(item => item.id === Number(id));
+  //   if (index !== -1) {
+  //     return this.data.splice(index, 1).length;
+  //   } else {
+  //     return 0;
+  //   }
+  // }
 
-  findByIdAndUpdate(id, update) {
-    let item = this.findById(Number(id));
-    if (item) {
-      Object.assign(item, update);
-    }
-    return item;
-  }
+  // findByIdAndUpdate(id, update) {
+  //   let item = this.findById(Number(id));
+  //   if (item) {
+  //     Object.assign(item, update);
+  //   }
+  //   return item;
+  // }
 
 }

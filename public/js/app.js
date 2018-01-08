aa028a01767610d2a3f1387035edea1ee01c24a5
Sam Gould <sjg326@gmail.com>
2018-01-07 19:38:11 -0800

    built functionality for chamber and party filters

--- public/js/app.js
+++ public/js/app.js
@@ -16,12 +16,32 @@ $(() => {
 
 $('#js-filter').click(function(event){
   event.preventDefault();
+  
+  const partyFilter = $('#party').val();
+  const chamberFilter = $('#chamber').val();
   const incumbentCheckbox = document.getElementById('incumbent');
+
+  $('.js-candidate-id-element').show();
+
+  if (partyFilter === 'democrat'){
+    $('.R').hide();
+    $('.I').hide();
+  } else if (partyFilter === 'republican'){
+    $('.D').hide();
+    $('.I').hide();
+  } else if (partyFilter === 'independent'){
+    $('.D').hide();
+    $('.R').hide();
+  }
+
+  if (chamberFilter === 'house'){
+    $('.Senate').hide();
+  } else if (chamberFilter === 'senate'){
+    $('.House').hide();
+  }
+  
   if (incumbentCheckbox.checked){
     $('.challenger').hide();
-  }
-  else {
-    $('.challenger').show();
   }
 });
 
